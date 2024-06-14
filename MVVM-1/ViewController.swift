a4c91603b00206a86bd2d1f4234e9b2300f45600
Nurgali Nuraly <nurali.nurgali@gmail.com>
2024-06-14 16:05:04 +0500

    Initial Commit

---
+++ MVVM-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-1
+//
+//  Created by Nurgali on 14.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
